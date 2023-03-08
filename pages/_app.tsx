c6f98137a809d2b39038ea725645a0921025328c
Mateus Souza <mateus_souza_teles@hotmail.com>
2023-03-07 21:51:28 -0300

    Create global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import "../styles/globals.css";
+import GlobalStyles from "../styles/globalStyles";
 import { light } from "../styles/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={light}>
       <Component {...pageProps} />
+      <GlobalStyles />
     </ThemeProvider>
   );
 }
