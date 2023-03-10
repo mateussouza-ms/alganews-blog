fa753343d1a18bf6530852bbe4244d3486f8167c
Mateus Souza <mateus_souza_teles@hotmail.com>
2023-03-09 23:21:00 -0300

    Add remark-gfm plugin

--- components/Markdown/index.tsx
+++ components/Markdown/index.tsx
@@ -1,9 +1,14 @@
 import ReactMarkdown from "react-markdown";
+import gfm from "remark-gfm";
 
 interface MarkdownProps {
   children: string;
 }
 
 export function Markdown({ children }: MarkdownProps) {
-  return <ReactMarkdown className="MarkdownRender">{children}</ReactMarkdown>;
+  return (
+    <ReactMarkdown className="MarkdownRender" remarkPlugins={[gfm]}>
+      {children}
+    </ReactMarkdown>
+  );
 }
