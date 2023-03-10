ccee4c55d6afffe9f3aa579427361afbea86923d
Mateus Souza <mateus_souza_teles@hotmail.com>
2023-03-09 23:53:30 -0300

    Add syntax highlight on Markdown component

--- components/Markdown/index.tsx
+++ components/Markdown/index.tsx
@@ -1,5 +1,27 @@
-import ReactMarkdown from "react-markdown";
+import ReactMarkdown, { Components } from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { okaidia } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import gfm from "remark-gfm";
+
+const components: Components = {
+  code({ node, inline, className, children, ...props }) {
+    const match = /language-(\w+)/.exec(className || "");
+    return !inline && match ? (
+      <SyntaxHighlighter
+        style={okaidia as any}
+        language={match[1]}
+        PreTag="div"
+        {...props}
+      >
+        {String(children).replace(/\n$/, "")}
+      </SyntaxHighlighter>
+    ) : (
+      <code className={className} {...props}>
+        {children}
+      </code>
+    );
+  },
+};
 
 interface MarkdownProps {
   children: string;
@@ -7,7 +29,11 @@ interface MarkdownProps {
 
 export function Markdown({ children }: MarkdownProps) {
   return (
-    <ReactMarkdown className="MarkdownRender" remarkPlugins={[gfm]}>
+    <ReactMarkdown
+      className="MarkdownRender"
+      remarkPlugins={[gfm]}
+      components={components}
+    >
       {children}
     </ReactMarkdown>
   );
